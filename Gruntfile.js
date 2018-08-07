03b844ef4781755828ff3f95a629301d4fd36442
Michael Moore <michael.moore@contrastsecurity.com>
2018-08-07 10:08:08 -0400

    :wrench: update glob

--- Gruntfile.js
+++ Gruntfile.js
@@ -15,9 +15,7 @@ module.exports = function(grunt) {
 
     watch: {
       assemble: {
-        files: [
-          "<%= config.src %>/{content,data,templates}/{,*/}*.{md,hbs,yml}"
-        ],
+        files: ["<%= config.src %>/{data,templates}/{,*/}*.{md,hbs,yml}"],
         tasks: ["assemble"]
       },
       livereload: {
